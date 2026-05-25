cfc8e44948ac6b799d3ba344a050cbfb4100ff9a
BYnanfei <2963557469@qq.com>
2026-05-25 21:40:56 +0800

    图库管理平台2.0

--- src/main/java/com/gzlg/PictureManagementApplication.java
+++ src/main/java/com/gzlg/PictureManagementApplication.java
@@ -1,11 +1,9 @@
 package com.gzlg;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.gzlg.mapper")
 public class PictureManagementApplication {
 
     public static void main(String[] args) {
